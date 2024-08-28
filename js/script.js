b01e0b1d2dd296a530e28912e1af0a5e64d21b96
Rian Riana <rianrianabkm@gmail.com>
2024-08-28 13:50:23 +0700

    updates

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
 
     //meambahkan perintah ketika click
     anchor.addEventListener('click', function(e) {
-        //mencegah tindakan default dari navigasi
-        e.preventDefault();
+        //membuat opacity menjadi 0 untuk semua section
+        document.querySelectorAll('section').forEach(section => {
+            section.style.opacity = '0';
+            
         });
 
         // menambahkan waktu tunggu (loading untuk menjalankan fungsi berikutnya)
@@ -13,8 +15,8 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
             document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
             });
-        }, );
+            // membat opacity kembali terlihat untuk atribut href (halaman yang dituju)
+            document.querySelector(this.getAttribute('href')).style.opacity = '1';
+        }, 500);
     });
 });
-
-document.getElementById('name-banner').textContent = name
